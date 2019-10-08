e21343a2fb28a526fa5b0e2e99797505686a5c5a
Pavel Bolshakov <pawa.pochta@gmail.com>
2019-10-08 14:13:11 +0300

    Hash validation with all errors

--- HTP/HashValidation.js
+++ HTP/HashValidation.js
@@ -3,8 +3,8 @@
 const person = {
     address: 'Grodno',
     firstName: 'Ivan',
-    lastName: 'Pupkin',
-    age: 34,
+    lastName: 'Ivanov',
+    age: 30,
     weight: 50,
 };
 
@@ -16,24 +16,61 @@ const PersonSchema = {
     weight: 'number',
 };
 
-const validateBySchema = (hash, schema) => {
-    let result = true;
-
-    if (Object.keys(schema).length > Object.keys(hash).length) {
-        console.log('Не заполнено одно поле!');
-        return false;
-    } else if (Object.keys(schema).length < Object.keys(hash).length) {
-        console.log('Введено лишнее значение!');
-        return false;
-    }
-
-    for (let key in hash) {
-        if (typeof hash[key] !== schema[key]) {
-            console.log(`Поле ${key} заполнено некорректно!`);
-            return false;
-        }
-    }
-    return result;
+const validateData = {
+    extraKeys: [],
+    missingKeys: [],
+    wrongVal: [],
+    correctVal: [],
 };
 
-console.log(validateBySchema(person, PersonSchema));
+const result = (validateData) => {
+    let message = 'In this hash ';
+    if (validateData.extraKeys.length === 1) {
+        message = message + `has an extra key '${validateData.extraKeys}'\n`;
+    } else if (validateData.extraKeys.length > 1) {
+        message = message + `has an extra keys: '${validateData.extraKeys}'\n`;
+    }
+    if (validateData.missingKeys.length === 1) {
+        message = message + `schema key '${validateData.missingKeys}' is missing\n`;
+    } else if (validateData.missingKeys.length > 1) {
+        message = message + `schema keys: '${validateData.missingKeys}' are missing\n`;
+    }
+    if (validateData.wrongVal.length === 1) {
+        message = message + `key '${validateData.wrongVal}' entered incorrectly\n`;
+    } else if (validateData.wrongVal.length > 1) {
+        message = message + `keys: '${validateData.wrongVal}' entered incorrectly\n`;
+    }
+    if (validateData.correctVal.length === 1) {
+        message = message + `key '${validateData.correctVal}' match the value\n`;
+    } else if (validateData.correctVal.length > 1) {
+        message = message + `keys: '${validateData.correctVal}' match the values\n`;
+    }
+    return console.log(message);
+};
+
+const validateBySchema = (hash, schema) => {
+    let schemaKey = Object.keys(schema),
+        hashKey = Object.keys(hash);
+    if (schemaKey.length !== hashKey.length) {
+        console.log('Here even the number of fields does not match, but');
+    }
+    for (let key in schema) {
+        if (!hashKey.includes(key)) {
+            validateData.missingKeys.push(key);
+        }
+    }
+    for (let key in hash) {
+        if (schemaKey.includes(key)) {
+            if (typeof hash[key] !== schema[key]) {
+                validateData.wrongVal.push(key);
+            } else {
+                validateData.correctVal.push(key);
+            }
+        } else  {
+            validateData.extraKeys.push(key);
+        }
+    }
+    return result(validateData);
+};
+
+validateBySchema(person, PersonSchema);
